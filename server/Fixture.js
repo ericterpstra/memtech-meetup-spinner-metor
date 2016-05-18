272048573bcd8252d7215be6a925a73595d44dd8
Eric Terpstra <eterps@gmail.com>
2016-05-17 21:44:42 -0500

    Upgrade Meteor to 1.3.2.4

--- server/Fixture.js
+++ server/Fixture.js
@@ -1,10 +1,1 @@
 import Meetup from './imports/Meetup';
-
-if( MeetupsCollection.find({}).count() <= 0 ) {
-    let meetup = {
-        data: resMeetup.getMeetups(),
-        time: new Date()
-    };
-
-    MeetupsCollection.insert(meetup);
-}
